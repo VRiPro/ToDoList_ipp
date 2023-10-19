5aca057b273245b57f70dbcdebe8a05cd62616d7
ahmedbellaaj10 <ahmed-bellaaj@outlook.com>
2023-10-19 17:28:00 +0200

    add docstrings

--- src/project_to_do/task.py
+++ src/project_to_do/task.py
@@ -1,18 +1,27 @@
 from datetime import datetime
 
-
 class Task:
+    """Represents a task with attributes for identification, name, and description.
+
+    This class provides methods to mark tasks as completed and update their name and description.
+
+    Attributes:
+        task_id (int): Unique identifier for the task.
+        name (str): Name of the task.
+        description (str): Description of the task.
+        date (datetime): Date of task creation.
+        modif_date (datetime): Date of the last modification.
+        completed (bool): Indicates if the task is completed.
+        category (str): Category of the task.
+        priority (str): Priority level of the task.
+    """
     def __init__(self, task_id, name, description):
-        """_summary_
+        """Initialize a task.
 
         Args:
-            task_id (_type_): _description_
-            name (_type_): _description_
-            description (_type_): _description_
-            date (_type_): _description_
-            state (_type_): _description_
-            category (_type_): _description_
-            priority (_type_): _description_
+            task_id (int): Unique identifier for the task.
+            name (str): Name of the task.
+            description (str): Description of the task.
         """
         self.task_id = task_id
         self.name = name
@@ -22,108 +31,73 @@ class Task:
         self.completed = False
         self.category = ""
         self.priority = ""
-        # shall we add deadline ?
 
     def complete(self):
-        """Set the task to completed
-        """
+        """Mark the task as completed."""
         self.completed = True
 
     def update(self, name, description):
-        """_summary_
+        """Update the task's name and description.
 
         Args:
-            name (_type_): _description_
-            description (_type_): _description_
+            name (str): New name for the task.
+            description (str): New description for the task.
         """
         self.name = name
         self.description = description
         self.modif_date = datetime.now()
 
 
-class CriticalTask(Task):
-    def __init__(self, task_id, name, description, deadline):
-        """_summary_
-
-        Args:
-            task_id (_type_): _description_
-            name (_type_): _description_
-            description (_type_): _description_
-            date (_type_): _description_
-            state (_type_): _description_
-            category (_type_): _description_
-            priority (_type_): _description_
-        """
-        super().__init__(task_id, name, description)
-        self.priority = "Critical"
-        self._CriticalTask__deadline = datetime.strptime(deadline, "%Y-%m-%d")
-
-    @property
-    def deadline(self):
-        return self._CriticalTask__deadline
-    
-    @deadline.setter
-    def deadline(self, deadline):
-        """Setter of the deadline attribute
-        """
-        if isinstance(deadline, datetime):
-            self._CriticalTask__deadline = deadline
-        else:
-            self._CriticalTask__deadline = datetime.strptime(deadline, "%Y-%m-%d")
-
-    
-
 class TaskList:
+    """Manages a list of tasks with methods to add, complete, remove, and display tasks.
+
+    Attributes:
+        tasks (list): List of tasks.
+        task_id_counter (int): Counter to track the next available task ID.
+    """
     def __init__(self):
-        """_summary_
-        """
+        """Initialize a task list."""
         self.tasks = []
         self.task_id_counter = 1
 
     def add_task(self, name, description):
-        """ Add a task in the list of tasks
-            Update of the next task id
+        """Add a task to the list and update the task ID.
 
         Args:
-            name (_type_): _description_
-            description (_type_): _description_
+            name (str): Name of the task.
+            description (str): Description of the task.
         """
         task = Task(self.task_id_counter, name, description)
         self.tasks.append(task)
         self.task_id_counter += 1
 
     def complete_task(self, name):
-        """_summary_
-        Complete the first task with the name=="name"
-        Shall we select with the id if more than one have the same name ?
+        """Mark the first task with the given name as completed.
+
         Args:
-            name (_type_): _description_
+            name (str): Name of the task to complete.
         """
         for task in self.tasks:
             if task.name == name:
                 task.complete()
                 break
 
-    def remove_task(self, name):
-        """_summary_
-        Remove the first task with the name=="name"
-        Shall we select with the id if more than one have the same name ?
+    def remove_task(self, task_id):
+        """Remove a task by its ID.
+
         Args:
-            name (_type_): _description_
+            task_id (int): ID of the task to remove.
         """
         task_to_remove = None
         for task in self.tasks:
-            if task.name == name:
+            if task.task_id == task_id:
                 task_to_remove = task
                 break
 
         self.tasks.remove(task_to_remove)
 
     def display_tasks(self):
-        """ Display task if the list is not empty
-            Else disply no task
-
-        """
+        """Display tasks in the list, or indicate no tasks if the list is empty."""
         if len(self.tasks) == 0:
             print("No tasks")
             return "No tasks"
@@ -137,3 +111,51 @@ class TaskList:
                 print(
                     f"{task.task_id}\t{task.name}\t{task.description}\t{task.completed}")
             return result
+
+
+class CriticalTask(Task):
+    """Represents a critical task that inherits from Task.
+
+    This class extends the Task class with a 'Critical' priority level and a deadline attribute.
+
+    Args:
+        task_id (int): Unique identifier for the task.
+        name (str): Name of the task.
+        description (str): Description of the task.
+        deadline (str or datetime): Deadline for the critical task (in the format "%Y-%m-%d" or as a datetime object).
+
+    Attributes:
+        deadline (datetime): Deadline for the critical task.
+
+    Note:
+        The 'deadline' attribute can be set as a string in "%Y-%m-%d" format or as a datetime object.
+    """
+    def __init__(self, task_id, name, description, deadline):
+        """Initialize a critical task with a 'Critical' priority level and a deadline.
+
+        Args:
+            task_id (int): Unique identifier for the task.
+            name (str): Name of the task.
+            description (str): Description of the task.
+            deadline (str or datetime): Deadline for the critical task (in the format "%Y-%m-%d" or as a datetime object).
+        """
+        super().__init__(task_id, name, description)
+        self.priority = "Critical"
+        self._CriticalTask__deadline = datetime.strptime(deadline, "%Y-%m-%d")
+
+    @property
+    def deadline(self):
+        """Getter for the deadline attribute."""
+        return self._CriticalTask__deadline
+    
+    @deadline.setter
+    def deadline(self, deadline):
+        """Setter for the deadline attribute.
+
+        Args:
+            deadline (str or datetime): Deadline for the critical task (in the format "%Y-%m-%d" or as a datetime object).
+        """
+        if isinstance(deadline, datetime):
+            self._CriticalTask__deadline = deadline
+        else:
+            self._CriticalTask__deadline = datetime.strptime(deadline, "%Y-%m-%d")
